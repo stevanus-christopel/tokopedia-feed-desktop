261af0f3117fbe78f3bee3c19a77f9884c67ae38
Davin Iskandar <davin.iskandar@tokopedia.com>
2017-05-24 18:23:54 +0700

    add 'gulp bundle' to bundle all js inside route and all css

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,38 @@ var gulp = require("gulp"),//http://gulpjs.com/
 	autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
 	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
 	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+	concatCss = require('gulp-concat-css');//https://www.npmjs.com/package/gulp-concat-css
+	concat = require('gulp-concat');//https://www.npmjs.com/package/gulp-concat
 	log = util.log;
 
 var sassFiles = "./src/routes/Feed/components/FeedView.scss";
 
-gulp.task("sass", function(){
+gulp.task("sass", function() {
 	log("Generate CSS files " + (new Date()).toString());
-    gulp.src(sassFiles)
-		.pipe(sass({ style: 'expanded' }))
-					.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-		.pipe(gulp.dest('./src/routes/Feed/components'));
-	
 	gulp.src(sassFiles)
-		.pipe(sass({ style: 'expanded' }))
-					.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
-		.pipe(gulp.dest('./dist'));
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+	.pipe(gulp.dest('./src/routes/Feed/components'));
+
+	gulp.src(sassFiles)
+	.pipe(sass({ style: 'expanded' }))
+				.pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
+	.pipe(gulp.dest('./dist'));
 });
 
-gulp.task("watch", function(){
+gulp.task("watch", function() {
 	log("Watching scss files for modifications");
 	gulp.watch(sassFiles, ["sass"]);
 });
+
+gulp.task("bundle", function() {
+	log("Bundling css files");
+	gulp.src(['./public/css/*.css', './src/routes/Feed/components/FeedView.css'])
+    .pipe(concatCss("dv3-feed.css"))
+    .pipe(gulp.dest('./dist'));
+
+	log("Bundling all js files from routes");
+	gulp.src('./src/routes/**/*.js')
+    .pipe(concat('feed.js'))
+    .pipe(gulp.dest('./dist/'));
+})
