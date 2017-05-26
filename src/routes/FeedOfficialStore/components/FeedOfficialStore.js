67169d5f6e76b196ce31cb524d3e4dc126d19076
Davin Iskandar <davin.iskandar@tokopedia.com>
2017-05-26 18:02:47 +0700

    add FeedOfficialStore component

---
+++ src/routes/FeedOfficialStore/components/FeedOfficialStore.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './FeedOfficialStore.css';
+
+class FeedOfficialStore extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='feed-official-store'>
+				<h1 className='fw-600 fs-16 lh-16 feed-official-store__title'>Official Store</h1>
+				<div className='row-fluid feed-official-store__showcase'>
+					<div className='span4 feed-official-store__items'>
+						<img src='http://placehold.it/170x170' className='feed-official-store__items-image'/>
+					</div>
+					<div className='span4 feed-official-store__items'>
+						<img src='http://placehold.it/170x170' className='feed-official-store__items-image'/>
+					</div>
+					<div className='span4 feed-official-store__items'>
+						<img src='http://placehold.it/170x170' className='feed-official-store__items-image'/>
+					</div>
+				</div>
+
+				<div className='row-fluid feed-official-store__showcase'>
+					<div className='span4 feed-official-store__items'>
+						<img src='http://placehold.it/170x170' className='feed-official-store__items-image'/>
+					</div>
+					<div className='span4 feed-official-store__items'>
+						<img src='http://placehold.it/170x170' className='feed-official-store__items-image'/>
+					</div>
+					<div className='span4 feed-official-store__items'>
+						<img src='http://placehold.it/170x170' className='feed-official-store__items-image'/>
+					</div>
+				</div>
+
+				<div className='ta-center row-fluid feed-official-store__link-more'>
+					<a href="#">Lihat Semua</a>
+				</div>
+      </div>
+    );
+  }
+}
+
+export default FeedOfficialStore;
