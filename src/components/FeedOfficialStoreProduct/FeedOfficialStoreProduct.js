56c85da6853fd387a09931773f31bdaf884feb77
Stevanus Christopel <me@stevanus.id>
2017-05-29 10:25:19 +0700

    Move FeedOfficialStoreProduct to General Component - Fix

--- src/components/FeedOfficialStoreProduct/FeedOfficialStoreProduct.js
+++ src/components/FeedOfficialStoreProduct/FeedOfficialStoreProduct.js
@@ -13,28 +13,28 @@ class FeedStore extends Component {
 
   render() {
     return (
-			<div className='feed-official-store mb-20'>
-			<h1 className='fw-normal fs-16 lh-20 pt-50 feed-official-store__title--text'>Produk Terpilih dari Official Store</h1>
-				<div className='feed-official-store__frame'>
-					<div className='feed-official-store__content'>					
-						<div className='row-fluid feed-official-store__row'>
-							<div className='span4 feed-official-store__items'>
-								<div className='feed-official-store__items-details'>
-									<img src="http://placehold.it/300x300" className='feed-official-store__image'/>
-									<div className='feed-official-store__details'>
-										<div className='fs-12 fw-600 lh-17 feed-official-store__items--name'>
+			<div className='feed-official-store-product mb-20'>
+			<h1 className='fw-normal fs-16 lh-20 pt-50 feed-official-store-product__title--text'>Produk Terpilih dari Official Store</h1>
+				<div className='feed-official-store-product__frame'>
+					<div className='feed-official-store-product__content'>					
+						<div className='row-fluid feed-official-store-product__row'>
+							<div className='span4 feed-official-store-product__items'>
+								<div className='feed-official-store-product__items-details'>
+									<img src="http://placehold.it/300x300" className='feed-official-store-product__image'/>
+									<div className='feed-official-store-product__details'>
+										<div className='fs-12 fw-600 lh-17 feed-official-store-product__items--name'>
 											<div className="detail__name js-ellipsis" data-js-ellipsis-limit="15">
 											Dr.Kevin Stylish and Comfortable Men Sne
 											</div>
 										</div>
-										<div className='pb-20 feed-official-store__items--price'>
+										<div className='pb-20 feed-official-store-product__items--price'>
 											<label className='fs-12 fw-600 orange-red'>Rp 249.950</label>
 										</div>
 									</div>
 								</div>
-								<div className='feed-official-store__items--store'>
-									<div className='span4 feed-official-store__image-container-store'>
-										<img src="http://placehold.it/30x30" className='feed-official-store__image-store'/>
+								<div className='feed-official-store-product__items--store'>
+									<div className='span4 feed-official-store-product__image-container-store'>
+										<img src="http://placehold.it/30x30" className='feed-official-store-product__image-store'/>
 									</div>
 									<div className='span8 pt-15'>
 										<label className='fs-11 text-black07'> Dr. Kevin Shoes</label>
@@ -42,21 +42,21 @@ class FeedStore extends Component {
 								</div>
 							</div>
 
-							<div className='span4 feed-official-store__items'>
-								<div className='feed-official-store__items-details'>
-									<img src="http://placehold.it/300x300" className='feed-official-store__image'/>
-									<div className='feed-official-store__details'>
-										<div className='fs-12 fw-600 lh-17 feed-official-store__items--name'>
+							<div className='span4 feed-official-store-product__items'>
+								<div className='feed-official-store-product__items-details'>
+									<img src="http://placehold.it/300x300" className='feed-official-store-product__image'/>
+									<div className='feed-official-store-product__details'>
+										<div className='fs-12 fw-600 lh-17 feed-official-store-product__items--name'>
 											Cincin tunangan kawin pernikahan berlian
 										</div>
-										<div className='pb-20 feed-official-store__items--price'>
+										<div className='pb-20 feed-official-store-product__items--price'>
 											<label className='fs-12 fw-600 orange-red'>Rp 3.999.000</label>
 										</div>
 									</div>
 								</div>
-								<div className='feed-official-store__items--store'>
-									<div className='span4 feed-official-store__image-container-store'>
-										<img src="http://placehold.it/30x30" className='feed-official-store__image-store'/>
+								<div className='feed-official-store-product__items--store'>
+									<div className='span4 feed-official-store-product__image-container-store'>
+										<img src="http://placehold.it/30x30" className='feed-official-store-product__image-store'/>
 									</div>
 									<div className='span8 pt-15'>
 										<label className='fs-11 text-black07'> Tiaria</label>
@@ -64,21 +64,21 @@ class FeedStore extends Component {
 								</div>
 							</div>
 
-							<div className='span4 feed-official-store__items'>
-								<div className='feed-official-store__items-details'>
-									<img src="http://placehold.it/300x300" className='feed-official-store__image'/>
-									<div className='feed-official-store__details'>
-										<div className='fs-12 fw-600 lh-17 feed-official-store__items--name'>
+							<div className='span4 feed-official-store-product__items'>
+								<div className='feed-official-store-product__items-details'>
+									<img src="http://placehold.it/300x300" className='feed-official-store-product__image'/>
+									<div className='feed-official-store-product__details'>
+										<div className='fs-12 fw-600 lh-17 feed-official-store-product__items--name'>
 											Anker powercore 13400 mah premium black
 										</div>
-										<div className='pb-20 feed-official-store__items--price'>
+										<div className='pb-20 feed-official-store-product__items--price'>
 											<label className='fs-12 fw-600 orange-red'>Rp 944.800</label>
 										</div>
 									</div>
 								</div>
-								<div className='feed-official-store__items--store'>
-									<div className='span4 feed-official-store__image-container-store'>
-										<img src="http://placehold.it/30x30" className='feed-official-store__image-store'/>
+								<div className='feed-official-store-product__items--store'>
+									<div className='span4 feed-official-store-product__image-container-store'>
+										<img src="http://placehold.it/30x30" className='feed-official-store-product__image-store'/>
 									</div>
 									<div className='span8 pt-15'>
 										<label className='fs-11 text-black07'> Anker Indonesia</label>
@@ -89,24 +89,24 @@ class FeedStore extends Component {
 							
 
 
-						<div className='row-fluid feed-official-store__row'>
-							<div className='span4 feed-official-store__items'>
-								<div className='feed-official-store__items-details'>
-									<img src="http://placehold.it/300x300" className='feed-official-store__image'/>
-									<div className='feed-official-store__details'>
-										<div className='fs-12 fw-600 lh-17 feed-official-store__items--name'>
+						<div className='row-fluid feed-official-store-product__row'>
+							<div className='span4 feed-official-store-product__items'>
+								<div className='feed-official-store-product__items-details'>
+									<img src="http://placehold.it/300x300" className='feed-official-store-product__image'/>
+									<div className='feed-official-store-product__details'>
+										<div className='fs-12 fw-600 lh-17 feed-official-store-product__items--name'>
 											<div className="detail__name js-ellipsis" data-js-ellipsis-limit="15">
 											Dr.Kevin Stylish and Comfortable Men Sne
 											</div>
 										</div>
-											<div className='pb-20 feed-official-store__items--price'>
+											<div className='pb-20 feed-official-store-product__items--price'>
 												<label className='fs-12 fw-600 orange-red'>Rp 249.950</label>
 											</div>
 									</div>
 								</div>
-								<div className='feed-official-store__items--store'>
-									<div className='span4 feed-official-store__image-container-store'>
-										<img src="http://placehold.it/30x30" className='feed-official-store__image-store'/>
+								<div className='feed-official-store-product__items--store'>
+									<div className='span4 feed-official-store-product__image-container-store'>
+										<img src="http://placehold.it/30x30" className='feed-official-store-product__image-store'/>
 									</div>
 									<div className='span8 pt-15'>
 										<label className='fs-11 text-black07'> Dr. Kevin Shoes</label>
@@ -114,21 +114,21 @@ class FeedStore extends Component {
 								</div>
 							</div>
 
-							<div className='span4 feed-official-store__items'>
-								<div className='feed-official-store__items-details'>
-									<img src="http://placehold.it/300x300" className='feed-official-store__image'/>
-									<div className='feed-official-store__details'>
-										<div className='fs-12 fw-600 lh-17 feed-official-store__items--name'>
+							<div className='span4 feed-official-store-product__items'>
+								<div className='feed-official-store-product__items-details'>
+									<img src="http://placehold.it/300x300" className='feed-official-store-product__image'/>
+									<div className='feed-official-store-product__details'>
+										<div className='fs-12 fw-600 lh-17 feed-official-store-product__items--name'>
 											Cincin tunangan kawin pernikahan berlian
 										</div>
-										<div className='pb-20 feed-official-store__items--price'>
+										<div className='pb-20 feed-official-store-product__items--price'>
 											<label className='fs-12 fw-600 orange-red'>Rp 3.999.000</label>
 										</div>
 									</div>
 								</div>
-								<div className='feed-official-store__items--store'>
-									<div className='span4 feed-official-store__image-container-store'>
-										<img src="http://placehold.it/30x30" className='feed-official-store__image-store'/>
+								<div className='feed-official-store-product__items--store'>
+									<div className='span4 feed-official-store-product__image-container-store'>
+										<img src="http://placehold.it/30x30" className='feed-official-store-product__image-store'/>
 									</div>
 									<div className='span8 pt-15'>
 										<label className='fs-11 text-black07'> Tiaria</label>
@@ -136,21 +136,21 @@ class FeedStore extends Component {
 								</div>
 							</div>
 
-							<div className='span4 feed-official-store__items'>
-								<div className='feed-official-store__items-details'>
-									<img src="http://placehold.it/300x300" className='feed-official-store__image'/>
-									<div className='feed-official-store__details'>
-										<div className='fs-12 fw-600 lh-17 feed-official-store__items--name'>
+							<div className='span4 feed-official-store-product__items'>
+								<div className='feed-official-store-product__items-details'>
+									<img src="http://placehold.it/300x300" className='feed-official-store-product__image'/>
+									<div className='feed-official-store-product__details'>
+										<div className='fs-12 fw-600 lh-17 feed-official-store-product__items--name'>
 											Anker powercore 13400 mah premium black
 										</div>
-										<div className='pb-20 feed-official-store__items--price'>
+										<div className='pb-20 feed-official-store-product__items--price'>
 											<label className='fs-12 fw-600 orange-red'>Rp 944.800</label>
 										</div>
 									</div>
 								</div>
-								<div className='feed-official-store__items--store'>
-									<div className='span4 feed-official-store__image-container-store'>
-										<img src="http://placehold.it/30x30" className='feed-official-store__image-store'/>
+								<div className='feed-official-store-product__items--store'>
+									<div className='span4 feed-official-store-product__image-container-store'>
+										<img src="http://placehold.it/30x30" className='feed-official-store-product__image-store'/>
 									</div>
 									<div className='span8 pt-15'>
 										<label className='fs-11 text-black07'> Anker Indonesia</label>
@@ -159,7 +159,7 @@ class FeedStore extends Component {
 							</div>
 						</div>
 
-						<div className='ta-center row-fluid feed-official-store__link-more'>
+						<div className='ta-center row-fluid feed-official-store-product__link-more'>
 							<a href="#">Lihat Semua</a>
 						</div>
 
