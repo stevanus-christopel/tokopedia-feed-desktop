1d2254354a162b7acf4718e56a6bfed89aee45ac
Davin Iskandar <davin.iskandar@tokopedia.com>
2017-06-02 12:01:24 +0700

    add share button

--- src/components/FeedTopAdsShop/FeedTopAdsShop.js
+++ src/components/FeedTopAdsShop/FeedTopAdsShop.js
@@ -16,7 +16,12 @@ var FeedTopAdsShop = React.createClass({
                 <span className='fw-600'>Makeup Forever </span>
                 <div className='fs-11 feed-topads-shop__seller-activity--time'>Bukan Produk Biasa</div>
               </div>
-              <div className='feed-topads-shop__seller-share'></div>
+              <div className='feed-topads-shop__seller-share'>
+                <button className='btn'>
+                  <img alt='' src={getImage('icon-plus.png')} />
+                   Favoritkan
+                </button>
+              </div>
             </div>
 
             <div className='feed-topads-shop__content'>
