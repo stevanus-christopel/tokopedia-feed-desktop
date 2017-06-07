eb61a2ff2dca0e4582be72e84b526e5972b4006a
Davin Iskandar <davin.iskandar@tokopedia.com>
2017-06-07 11:54:52 +0700

    ButtonShare hover fix

--- src/components/ButtonShare/ButtonShare.js
+++ src/components/ButtonShare/ButtonShare.js
@@ -8,26 +8,26 @@ var ButtonShare = React.createClass({
       isShowPopover: false
     };
   },
-  togglePopover: function() {
-    if(!this.state.isShowPopover) {
-      document.onmouseover = this.togglePopover;
-    } else {
-      document.onmouseover = null;
-    }
+  // togglePopover: function() {
+  //   if(!this.state.isShowPopover) {
+  //     document.getElementsByClassName('button-share__icon-container').onmouseover = this.togglePopover;
+  //   } else {
+  //     document.getElementsByClassName('button-share__icon-container').onmouseover = null;
+  //   }
 
-    this.setState({
-      isShowPopover: !this.state.isShowPopover
-    });
-  },
+  //   this.setState({
+  //     isShowPopover: !this.state.isShowPopover
+  //   });
+  // },
   render: function() {
     return (
-      <div>
-        <button className='btn button-share' onMouseOver={() => this.togglePopover()}>
+      <div onMouseLeave={() => this.setState({isShowPopover: false})}>
+        <button className='btn button-share' onMouseOver={() => this.setState({isShowPopover: true})}>
             <img className='button-share--icon' alt='' src={getImage('icon-btn-share.png')} /> Bagikan
         </button>
         {
           this.state.isShowPopover === true &&
-          <div className='button-share__icon-container'>
+          <div className='button-share__icon-container' >
             <img className='button-share--arrow' alt='' src={getImage('arrow-up.png')} />
             <img className='button-share--link' alt='' src={getImage('icon-fb.png')} />
             <img className='button-share--link' alt='' src={getImage('icon-twitter.png')} />
